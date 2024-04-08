e39bd0428f9fec74b6cfe1b4a0e70be8c0c7b2cc
Filipp <wolverinexdd@yandex.ru>
2024-04-09 01:00:39 +0400

    second_proj_popup_com

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+import { initialCards } from '../scripts/cards.js';
+import '../src/index.css'; // добавьте импорт главного файла стилей
 const placesList = document.querySelector('.places__list');
+const editPopupButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const popup = document.querySelectorAll('.popup')
+
+function openPopup(popupElement) {
+  popupElement.classList.add('.popup_is-opened');
+}
+
+editPopupButton.addEventListener('click', function () {
+  openPopup(popup);
+});
+
+addButton.addEventListener('click', function () {
+  openPopup(popup);
+});
+
 function addCard(card, onRemoveCard) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
   const cardImage = cardItem.querySelector('.card__image');
-  deleteButton = cardItem.querySelector('.card__delete-button');
+  const deleteButton = cardItem.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', onRemoveCard);
   cardItem.querySelector('.card__title').textContent = card.name;
   cardImage.src = card.link;
